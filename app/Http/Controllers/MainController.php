cf7cd06d55383df9becd15fafd9745cca2166bda
daskafa <ydaskafa@gmail.com>
2021-06-03 19:09:42 +0300

    more clean

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Category;
-use App\Models\Main;
 use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
@@ -13,11 +12,21 @@ class MainController extends Controller
 
     public function show($code)
     {
-        $q = DB::table('mains')->where('statu', $code)->get();
-        $category = $q[0]->category_id;
+        $query = DB::table('mains')->where('statu', $code)->get();
+        if (empty($query[0])){
+            return response()->json([
+                'Hata' => 'Belirtilen Statu Kodu Mevcut Değil veya Kayıtlı Değil'
+            ], 404);
+        }
+        $q = $query[0];
+        $category = Category::find($q->category_id);
         return response()->json([
-            'Sorgu' => $q,
-            'kategori' => Category::find($category)
+            'Girilen Statu' => $q->statu,
+            'Mesaj' => $q->message,
+            'Açıklama' => $q->information,
+            'Kategori' => $category->name,
+            'Kategori Açıklaması' => $category->information,
+            'Kaynak' => 'https://www.kukumav.net'
         ]);
 
     }
